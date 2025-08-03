65d3d942dff1afd3c6b0ed9c1136a1cf2d77217b
Michael Maddison <michaelmaddison@mac.mynetworksettings.com>
2025-08-03 00:25:09 -0400

    cleaned up animated info code

--- src/components/InfoButton.tsx
+++ src/components/InfoButton.tsx
@@ -5,33 +5,33 @@ import { motion } from 'motion/react';
 import InfoIcon from '@/components/icons/InfoIcon';
 import Image from 'next/image';
 
+const ANIMATION_CONFIG = {
+  duration: 0.15,
+  ease: "easeOut" as const
+};
+
+const INITIAL_STATE = { scale: 0, x: 0, y: 0, opacity: 0 };
+const RESET_STATE = { scale: 0, x: 0, y: 0, opacity: 0 };
+
 export default function InfoButton() {
   const [isInfoHovered, setIsInfoHovered] = React.useState(false);
 
   return (
     <div className="absolute bottom-4 right-4">
-      {/* Animated Rectangles */}
+      {/* Animated content containers */}
       
-      {/* Rectangle moving up */}
+      {/* Image container moving up */}
       <motion.div
-        className="absolute bottom-0 right-0 rounded-sm w-48 border-3 border-black z-1"
+        className="absolute bottom-0 right-0 rounded-sm w-48 border-2 border-black z-10"
         style={{ transformOrigin: 'bottom right' }}
-        initial={{ scale: 0, y: 0, x: 0, opacity: 0 }}
+        initial={INITIAL_STATE}
         animate={isInfoHovered ? { 
           scale: 1,
-          x: 0,//65,
+          x: 0,
           y: -200, 
           opacity: 1
-        } : { 
-          scale: 0, 
-          y: 0, 
-          x: 0,
-          opacity: 0 
-        }}
-        transition={{ 
-          duration: 0.15, 
-          ease: "easeOut"
-        }}
+        } : RESET_STATE}
+        transition={ANIMATION_CONFIG}
       >
         <Image 
           src="/images/home/yvonne-mike-mural-photo.png" 
@@ -42,56 +42,40 @@ export default function InfoButton() {
         />
       </motion.div>
       
-      {/* Rectangle moving left */}
+      {/* Image container moving left */}
       <motion.div
         className="absolute bottom-0 right-0 rounded-sm w-48 border-2 border-black z-0"
         style={{ transformOrigin: 'bottom right' }}
-        initial={{ scale: 0, x: 0, y: 0, opacity: 0 }}
+        initial={INITIAL_STATE}
         animate={isInfoHovered ? { 
           scale: 1, 
           x: -140, 
-          y: 15,
-          opacity: 1
-        } : { 
-          scale: 0, 
-          x: 0, 
           y: 0,
-          opacity: 0 
-        }}
-        transition={{ 
-          duration: 0.15,
-          ease: "easeOut"
-        }}
+          opacity: 1
+        } : RESET_STATE}
+        transition={ANIMATION_CONFIG}
       >
         <Image 
           src="/images/home/mike-painting-mural.png" 
-          alt="Image 2" 
+          alt="mike painting mural" 
           width={400}
           height={300}
           className="object-contain"
         />
       </motion.div>
 
-      {/* Rectangle moving left */}
+      {/* Text container moving diagonally */}
       <motion.div
-        className="absolute bottom-0 right-0 rounded-sm w-48 border-3 border-black z-3"
+        className="absolute bottom-0 right-0 rounded-sm w-48 border-2 border-black z-30"
         style={{ transformOrigin: 'bottom right' }}
-        initial={{ scale: 0, x: 0, y: 0, opacity: 0 }}
+        initial={INITIAL_STATE}
         animate={isInfoHovered ? { 
           scale: 1, 
           x: -170, 
           y: -170,
           opacity: 1
-        } : { 
-          scale: 0, 
-          x: 0, 
-          y: 0,
-          opacity: 0 
-        }}
-        transition={{ 
-          duration: 0.15,
-          ease: "easeOut"
-        }}
+        } : RESET_STATE}
+        transition={ANIMATION_CONFIG}
       >
         <div className="font-stamp-one text-sm text-white bg-black opacity-70 p-4">This home page, including the aligator icon, attempts to digitize a mural that my good friend Yvonne Chen and I made in college. Inspired by Minna Leunig, we created a wall-to-wall mural of bumpy animals</div>
       </motion.div>
