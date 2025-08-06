b4c4840e679f64457dc19887f67e4cb187a7bdd5
Michael Maddison <michaelmaddison@mac.mynetworksettings.com>
2025-08-05 20:27:09 -0400

    final fixes to info modal for mobile display

--- src/components/InfoButton.tsx
+++ src/components/InfoButton.tsx
@@ -3,13 +3,14 @@ import {
     Dialog,
     DialogContent,
     DialogDescription,
-    DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
   } from "@/components/ui/dialog"
 import { Badge } from "@/components/ui/badge"
 import ExternalLinkIcon from "@/components/icons/ExternalLinkIcon";
+import { Toaster } from "@/components/ui/sonner"
+import { toast } from "sonner"
 
 type InfoButtonStyle = {
     className?: string;
@@ -21,60 +22,66 @@ export default function InfoButton({ className = '' }: InfoButtonStyle) {
     const handleEmailClick = async () => {
         try {
             await navigator.clipboard.writeText('michael.maddison02@gmail.com');
+            toast.success('Email copied to clipboard!');
         } catch (err) {
             console.error('Failed to copy email: ', err);
+            toast.error('Failed to copy email');
         }
     };
 
     return (
-        <Dialog>
-            <DialogTrigger asChild className={className}>
-                <div className={className}>INFO</div>
-            </DialogTrigger>
+        <>
+            <Dialog>
+                <DialogTrigger asChild className={className}>
+                    <div className={className}>INFO</div>
+                </DialogTrigger>
 
-            <DialogContent className="bg-black/50 border-none font-crit text-white backdrop-blur-md">
-                <DialogHeader>
-                    <DialogTitle className="pb-2">{"Hi, I'm Michael Maddison"}</DialogTitle>
-                    <DialogDescription className="flex pb-2 font-crit text-white">
-                        {"I'm a software engineer, currently focusing on full-stack development, who is passionate about building impactful digital experiences. I love exploring the intersection of technology and user experience, always asking: how can we make systems that both feel frictionless and perform elegantly?"}
-                    </DialogDescription>
-                </DialogHeader>
-                <DialogDescription className="flex flex-wrap font-crit text-white gap-2">
-                    {skills.map((item, index) => (
-                        <Badge key={index} variant={"secondary"}>{item}</Badge>
-                    ))}
-                </DialogDescription>
+                <DialogContent className="bg-black/50 border-none font-crit text-white backdrop-blur-md">
+                    <DialogHeader>
+                        <DialogTitle className="text-start pb-2">{"Hi, I'm Michael Maddison"}</DialogTitle>
+                        <DialogDescription className="flex text-start font-crit text-white pb-2">
+                            {"I'm a software engineer, currently focusing on full-stack development, who is passionate about building impactful digital experiences. I love exploring the intersection of technology and user experience, always asking: how can we make systems that both feel frictionless and perform elegantly?"}
+                        </DialogDescription>
+                    </DialogHeader>
 
-                <DialogDescription className="flex flex-col sm:flex-row sm: justify-between items-start gap-2">
-                    <div className="flex flex-row items-start gap-4">
-                        <a 
-                            href="https://www.linkedin.com/in/michael-maddison/"
-                            target="_blank" 
-                            rel="noopener noreferrer"
-                            className="font-crit text-white opacity-80 hover:text-gray-300 transition-colors flex items-center gap-1"
-                            >
-                            LinkedIn
-                            <ExternalLinkIcon className="w-4 h-4" />
-                        </a>
-                        <a 
-                            href="https://github.com/michaelmaddison02"
-                            target="_blank" 
-                            rel="noopener noreferrer"
-                            className="font-crit text-white opacity-80 hover:text-gray-300 transition-colors flex items-center gap-1"
-                            >
-                            Github
-                            <ExternalLinkIcon className="w-4 h-4" />
-                        </a>
+                    <div className="flex flex-wrap font-crit text-white gap-2 pb-2">
+                        {skills.map((item, index) => (
+                            <Badge key={index} variant={"secondary"}>{item}</Badge>
+                        ))}
                     </div>
-                    <button 
-                        onClick={handleEmailClick}
-                        className="font-crit text-white opacity-80 hover:text-gray-300 transition-colors flex items-center gap-1 focus:outline-none focus:ring-0"
-                        >
-                        Michael.Maddison02@gmail.com
-                    </button>
-                </DialogDescription>
-            </DialogContent>
-        
-        </Dialog>
+
+                    <div className="flex flex-col sm:flex-row sm: justify-between items-start gap-2">
+                        <div className="flex flex-row items-start gap-4">
+                            <a 
+                                href="https://www.linkedin.com/in/michael-maddison/"
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                className="font-crit text-sm text-white opacity-80 hover:text-gray-300 transition-colors flex items-center gap-1"
+                                >
+                                LinkedIn
+                                <ExternalLinkIcon className="w-4 h-4" />
+                            </a>
+                            <a 
+                                href="https://github.com/michaelmaddison02"
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                className="font-crit text-sm text-white opacity-80 hover:text-gray-300 transition-colors flex items-center gap-1"
+                                >
+                                Github
+                                <ExternalLinkIcon className="w-4 h-4" />
+                            </a>
+                        </div>
+                        <button 
+                            onClick={handleEmailClick}
+                            className="font-crit text-sm text-white opacity-80 hover:text-gray-300 transition-colors flex items-center gap-1 focus:outline-none focus:ring-0"
+                            >
+                            Michael.Maddison02@gmail.com
+                        </button>
+                    </div>
+                </DialogContent>
+            
+            </Dialog>
+            <Toaster />
+        </>
     );
 }
